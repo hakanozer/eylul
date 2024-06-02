a8f11e0ff03172c52cb54265567b80a1384c4bc6
Hakan ÖZER <hakanozer02@gmail.com>
2024-06-02 14:59:23 +0300

    Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appDatas = {name : "Ali", surname: "Bilmem"}
+  email:string = ""
   title = 'peoject';
+
+  setEmail(email:string) {
+    this.email = email
+  }
+
+  
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  @Input()
+  datas!: {name: string, surname: string}
+
+  @Output()
+  sendEvent = new EventEmitter<string>()
+
+  fncSend(email: string) {
+    console.log(this.datas.name)
+    console.log(email)
+    this.sendEvent.emit(email)
+  }
 
   constructor() { }
 
